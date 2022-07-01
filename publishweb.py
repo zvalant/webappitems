84b9abd3e36ee9203f3eb1af0bc8e61fe2cf6d6d
zvalant <96396989+zvalant@users.noreply.github.com>
2022-07-01 16:00:44 -0500

    Add files via upload

---
+++ publishweb.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("Our First Streamlit App")
+st.subheader("Introducing Streamlit in Automate Everything with Python")
+st.write("""This is our first Web App.
+Enjoy!""")
+
